092b9e419c6006314f4c7230a943c79761138b68
ccaldarella99 <christopher.caldarella@outlook.com>
2018-02-01 17:54:19 -0700

    Spacing, arrangements, added elements

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+function showNavMenu() {
+	document.getElementById('nav-right-bottom').classList.toggle('hide-nav');
+}
+
+function hideNavMenu() {
+	//document.getElementById('nav-menu').classList.addClass('hide-nav');
+}
