0c9f1ed2ac02ded3a79e3b34beb8c8c29b2ce85a
ccaldarella99 <christopher.caldarella@outlook.com>
2018-03-13 20:54:04 -0600

    Hamburger Menu for Hosts page only

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+document.onkeydown = function(evt) {
+	evt = evt || window.event;
+	var isEscape = false;
+	if ("key" in evt) {
+		isEscape = (evt.key == "Escape" || evt.key == "Esc");
+	} else {
+		isEscape = (evt.keyCode == 27);
+	}
+	if (isEscape) {
+		closeMenu('menu-overlay','over-menu');
+	}
+};
 
 
-function showNavMenu() {
-	//document.getElementById('nav-right-bottom').classList.toggle('hide-nav');
+function openMenu(item, content) {
+	document.getElementById(item).style.width = "100%";
+	document.getElementById('hamburger').style.display = "none";
+	document.getElementById(content).style.display = "inline-block";
 }
 
-function hideNavMenu() {
-	//document.getElementById('nav-menu').classList.addClass('hide-nav');
+function closeMenu(item, content) {
+	document.getElementById(content).style.display = "none";
+	document.getElementById('hamburger').style.display = "inline-block";
+	document.getElementById(item).style.width = "0%";
 }
+
+
+
+
